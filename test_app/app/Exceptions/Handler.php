6da88d14638606d80a657d17b8cd0e2c48633edd
Lynn <lynn990908@gmail.com>
2025-04-03 16:39:00 +0800

    error handle

--- test_app/app/Exceptions/Handler.php
+++ test_app/app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -19,12 +21,27 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
+     * 參數驗證失敗時使用 400 並且回傳 json 格式(未改寫則走laravel預設422)
+     * 紀錄 錯誤日誌
+     * @return void
      */
     public function register(): void
     {
+        $this->renderable(function (ValidationException $e, $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => '請求參數驗證失敗',
+                    'errors' => $e->errors(),
+                ], 400);
+            }
+        });
         $this->reportable(function (Throwable $e) {
-            //
+            Log::error($e->getMessage(), [
+                'exception' => $e,
+                'url' => request()->fullUrl(),
+                'input' => request()->all()
+            ]);
         });
     }
 }
