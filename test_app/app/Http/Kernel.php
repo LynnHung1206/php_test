3c27b8e738d9849a8bc315871a9249ca1d0f8a01
Lynn <lynn.hung@104.com.tw>
2025-04-07 11:05:50 +0800

    demo adjust

--- test_app/app/Http/Kernel.php
+++ test_app/app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        //自定義中介層
         'token.auth' => \App\Http\Middleware\TokenAuthMiddleware::class,
     ];
 }
